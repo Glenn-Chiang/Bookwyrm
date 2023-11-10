05d5d313d1e712c87218365c0799b533c355da43
Glenn Chiang <glennchiang24@gmail.com>
2023-11-10 12:38:42 +0800

    Implemented extraction of relevant book data and rendering of book data in BookResult component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,25 +1,39 @@
 import { VolumeData, VolumeInfo } from "@/lib/books-api/types";
+import Image from "next/image";
 
 type SearchResultsProps = {
-  searchTerm: string | undefined
-}
+  searchTerm: string | undefined;
+};
 
-const getBooks = async (searchTerm: string) => {
-  const queryString = searchTerm?.split(' ').join('+')
+const getBooksData = async (searchTerm: string) => {
+  const queryString = searchTerm?.split(" ").join("+");
   const res = await fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${queryString}&key=${process.env.BOOKS_API_KEY}`
   );
-  const volumesData: VolumeData[] = (await res.json()).items
-  const books: VolumeInfo[] = volumesData.map(item => item.volumeInfo)
-  console.log(books)
-}
-
-export const SearchResults = async ({searchTerm}: SearchResultsProps) => {
-  const books = searchTerm ? await getBooks(searchTerm) : []
-
-  return (
-    <section>
-
-    </section>
-  )
+  const volumeResults: VolumeData[] = (await res.json()).items;
+  const books: VolumeInfo[] = volumeResults.map((item) => {
+    return {...item.volumeInfo, id: item.id}
+  });
+  return books
 };
+
+export const SearchResults = async ({ searchTerm }: SearchResultsProps) => {
+  const books = searchTerm ? await getBooksData(searchTerm) : [];
+
+  return <section>
+    {books.map(book => <BookResult key={book.id} book={book}/>)}
+  </section>;
+};
+
+const BookResult = ({book}: {book: VolumeInfo}) => {
+  const {title, authors, publisher, publishedDate, imageLinks} = book
+  return (
+    <article className="flex">
+      {imageLinks && imageLinks.thumbnail && <Image src={imageLinks.thumbnail} alt="" width={100} height={200}/>}
+      <div>
+        <div>{title}</div>
+        <div>{authors ? authors.join(', ') : '-'}</div>
+      </div>
+    </article>
+  )
+}
--- src/lib/books-api/types.ts
+++ src/lib/books-api/types.ts
@@ -1,7 +1,12 @@
+// Book data extracted from google-books-api that we are interested in
+export type BookInfo = {};
+
+// Raw data returned from google-books-api
 export type VolumeInfo = {
+  id: string;
   title: string;
   subtitle: string;
-  authors: string[];
+  authors?: string[];
   publisher: string;
   publishedDate: string;
   description: string;
@@ -23,7 +28,7 @@ export type VolumeInfo = {
   averageRating: number;
   ratingsCount: number;
   contentVersion: string;
-  imageLinks: {
+  imageLinks?: {
     smallThumbnail: string;
     thumbnail: string;
     small: string;
