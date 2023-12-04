eb9a7d327bc20d72dc3c62238f5ca64bea7b48c8
Glenn Chiang <glennchiang24@gmail.com>
2023-12-04 22:35:03 +0800

    Changed Navbar to server component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,11 @@
-"use client";
-
 import Link from "next/link";
 import { Logo } from "./Logo";
-import { useCurrentUser } from "@/lib/auth";
+import { getCurrentUser, useCurrentUser } from "@/lib/auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpenReader, faUsers } from "@fortawesome/free-solid-svg-icons";
 
-export const Navbar = () => {
-  const currentUser = useCurrentUser();
+export const Navbar = async () => {
+  const currentUser = await getCurrentUser()
   return (
     <nav className="fixed h-16 left-0 top-0 w-screen flex items-center justify-start gap-8 z-20 bg-sky-100 text-sky-500 px-2 font-medium">
       <Link href={"/"} className="flex gap-2 items-center text-xl ">
